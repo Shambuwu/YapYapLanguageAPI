6ec2e5678654602fc67111d7b994a10147cf63ec
Shambuwu <balfappius@protonmail.ch>
2026-02-09 23:21:58 +0100

    YapYapLanguageAPI Initialization

---
+++ YapYapLanguageAPI/Patch_StartVosk.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+
+[HarmonyPatch(typeof(VoskSpeechToText), "StartVosk",
+    new[] { typeof(List<string>), typeof(string), typeof(int) })]
+class Patch_StartVosk
+{
+    static void Prefix(ref List<string> grammer)
+    {
+        if (grammer == null) return;
+
+        if (!grammer.Contains("douwe klip"))
+            grammer.Add("douwe klip");
+
+        if (!grammer.Contains("сука"))
+            grammer.Add("сука");
+
+        if (!grammer.Contains("иди нахуй"))
+            grammer.Add("иди нахуй");
+
+        if (!grammer.Contains("clamboosterbeern"))
+            grammer.Add("clamboosterbeern");
+    }
+}
---
+++ YapYapLanguageAPI/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+[BepInPlugin("yapyap.language.api", "YapYap Language API", "0.1.0")]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        Logger.LogInfo("YapYapLanguageAPI loaded");
+
+        var harmony = new Harmony("yapyap.language.api");
+        harmony.PatchAll();
+    }
+}
